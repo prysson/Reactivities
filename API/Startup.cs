4514ad8d4044be2ade9e99418c00675ae221c6b4
Prysson <prysson@gmail.com>
2021-01-26 00:03:55 -0600

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -36,6 +43,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
